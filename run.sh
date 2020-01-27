d6a70fda2ece44c6cd84b22d4a9d13ebae6eeb1b
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-01-27 22:13:53 +0800

    [Update] run.sh
    ./run.sh server/client #thread_num

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ client = "client"
 server_ip = "172.18.13.15"
 server_port = 12345
 
-if test $0 = $server
+if test $1 = $server
 then
-    ./libevent_test $1
+    ./libevent_test $2
 else
     i = 0
     while($i < $1)
@@ -21,4 +21,4 @@ else
         cd ..
         let "i++"
     done
-    ./libevent_test $1 $server_ip $server_port
+    ./libevent_test $2 $server_ip $server_port
