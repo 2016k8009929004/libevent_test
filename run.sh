93ad690623ea445105ab2f81e18adb2b4e62db86
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-01-27 22:50:36 +0800

    Revert "[Update] run.sh"
    This reverts commit d6a70fda2ece44c6cd84b22d4a9d13ebae6eeb1b.

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ client=‘client’
 server_ip=‘172.18.13.15’
 server_port=12345
 
-if test $1 = $server
+if test $0 = $server
 then
-    ./libevent_test $2
+    ./libevent_test $1
 else
     i=0
     while($i < $2)
@@ -21,4 +21,4 @@ else
         cd ..
         let "i++"
     done
-    ./libevent_test $2 $server_ip $server_port
+    ./libevent_test $1 $server_ip $server_port
