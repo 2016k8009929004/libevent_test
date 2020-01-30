a29c202f2cf1cc90f73679d41698cc5055d1b526
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-01-30 15:11:31 +0800

    [test]

--- multi_thread/client.c
+++ multi_thread/client.c
@@ -29,7 +29,13 @@ int connect_server(char * server_ip, int port){
 }
 
 void * send_request(void * arg){
-    int fd = *((int *)arg);
+    struct send_info * info = (struct send_info *)arg;
+
+    int fd = *(info->sockfd);
+
+    pthread_mutex_t * send_lock = info->send_lock;
+
+    int * send_byte = info->send_byte;
 
     char send_buf[BUF_SIZE];
 	char recv_buf[BUF_SIZE + 1];
@@ -49,7 +55,10 @@ void * send_request(void * arg){
 			exit(1);
 		}
 
-        sent_byte += send_size;
+        pthread_mutex_lock(send_lock);
+        (*send_byte) += send_size;
+        pthread_mutex_unlock(send_lock);
+
 /*
 		recv_size = recv(fd, recv_buf, sizeof(recv_buf) - 1, 0);
 		if(recv_size < 0){
@@ -63,7 +72,7 @@ void * send_request(void * arg){
 
     }
 
-	printf("[CLIENT %d] request complete, sent byte: %d\n", fd, sent_byte);
+	printf("[CLIENT %d] request complete, send byte: %d\n", fd, *send_byte);
 
     fclose(send_fp);
 //    fclose(recv_fp);
@@ -74,11 +83,19 @@ void * send_request(void * arg){
 
 void response_process(int sock, short event, void * arg){
 #ifdef RECEIVE_DEBUG
-    struct response_process_arg * response_arg = (struct response_process_arg *)arg;
-    struct event * read_ev = response_arg->read_ev;
-    FILE * fp = response_arg->fp;
+    struct debug_response_arg * debug_arg = (struct debug_response_arg *)arg;
+
+    struct event * read_ev = debug_arg->read_ev;
+    FILE * fp = debug_arg->fp;
 #else
-    struct event * read_ev = (struct event *)arg;
+    struct response_arg * response_process_arg = (struct response_arg *)arg;
+
+    struct event * read_ev = response_process_arg->read_ev;
+    struct send_info * info = response_process_arg->info;
+
+    pthread_mutex_t * recv_lock = info->recv_lock;
+    int * recv_byte = info->recv_byte;
+    int * send_byte = info->send_byte;
 #endif
 
     char recv_buf[BUF_SIZE + 1];
@@ -97,12 +114,12 @@ void response_process(int sock, short event, void * arg){
 #endif
 
     pthread_mutex_lock(&recv_lock);
-    recv_byte += recv_size;
+    (*recv_byte) += recv_size;
     pthread_mutex_unlock(&recv_lock);
     
     printf("[CLIENT %d] receive reply: %s\n", sock, recv_buf);
 
-    if(recv_byte == sent_byte){
+    if((*recv_byte) == (*sent_byte)){
         printf("[CLIENT %d] receive reply complete, close connection\n", sock);
 
         pthread_mutex_lock(&fin_client_thread_lock);
@@ -122,7 +139,9 @@ void response_process(int sock, short event, void * arg){
 }
 
 void * create_response_process(void * arg){
-    int sock = *((int *)arg);
+    struct send_info * info = (struct send_info *)arg;
+
+    int fd = *(info->sockfd);
 
     struct event_base * base = event_base_new();
 
@@ -131,13 +150,17 @@ void * create_response_process(void * arg){
 #ifdef RECEIVE_DEBUG
     FILE * recv_fp = fopen("server-ouput.dat", "wb");
 
-    struct response_process_arg * response_arg = (struct response_process_arg *)malloc(sizeof(struct response_process_arg));
-    response_arg->read_ev = read_ev;
-    response_arg->fp = recv_fp;
+    struct debug_response_arg * debug_arg = (struct debug_response_arg *)malloc(sizeof(struct debug_response_arg));
+    debug_arg->read_ev = read_ev;
+    debug_arg->fp = recv_fp;
 
-    event_set(read_ev, sock, EV_READ|EV_PERSIST, response_process, response_arg);
+    event_set(read_ev, fd, EV_READ|EV_PERSIST, response_process, debug_arg);
 #else
-    event_set(read_ev, sock, EV_READ|EV_PERSIST, response_process, read_ev);
+    struct response_arg * response_process_arg = (struct response_arg *)malloc(RESPONSE_ARG_SIZE);
+    response_process_arg->read_ev = read_ev;
+    response_process_arg->info = info;
+
+    event_set(read_ev, fd, EV_READ|EV_PERSIST, response_process, response_process_arg);
 #endif
 
     event_base_set(base, read_ev);
@@ -147,44 +170,46 @@ void * create_response_process(void * arg){
     event_base_dispatch(base);
 }
 
-void receive_response_thread(int fd){
+void receive_response_thread(struct send_info * info){
     pthread_t thread;
-    pthread_create(&thread, NULL, create_response_process, (void *)&fd);
+    pthread_create(&thread, NULL, create_response_process, (void *)info);
     pthread_detach(thread);
 }
 
-void send_request_thread(int fd){
+void send_request_thread(struct send_info * info){
     pthread_t thread;
-    pthread_create(&thread, NULL, send_request, (void *)&fd);
+    pthread_create(&thread, NULL, send_request, (void *)info);
     pthread_detach(thread);
 }
 
 void * client_thread(void * argv){
     struct server_node * server = (struct server_node *)argv;
-//    char ** arg = (char **)argv;
+    
+    int send_byte, recv_byte;
+    send_byte = recv_byte = 0;
 
-//    printf("[CLIENT] server ip: %s, server port: %d\n", arg[2], atoi(arg[3]));
+    pthread_mutex_t send_lock, recv_lock;
+    pthread_mutex_init(&send_lock, NULL);
+    pthread_mutex_init(&recv_lock, NULL);
 
     int sockfd = connect_server(*(server->ip_addr), server->port);
-//    int sockfd = connect_server(arg[2], atoi(arg[3]));
     if(sockfd == -1){
         perror("[CLIENT] tcp connect error");
         exit(1);
     }
 
-    pthread_mutex_init(&recv_lock, NULL);
+    struct send_info * info = (struct send_info *)malloc(SEND_INFO_SIZE);
+    info->sockfd = &sockfd;
+    info->send_lock = &send_lock;
+    info->send_byte = &send_byte;
+    info->recv_lock = &recv_lock;
+    info->recv_byte = &recv_byte;
 
-//    printf("[CLIENT] connected to server\n");
+    receive_response_thread(info);
 
-    receive_response_thread(sockfd);
-
-    send_request_thread(sockfd);
-
-//    send_request(sockfd);
+    send_request_thread(info);
 
     while(!work_done_flag);
-
-//    while(1);
 
     return NULL;
 
--- multi_thread/include/client.h
+++ multi_thread/include/client.h
@@ -2,12 +2,6 @@
 
 #define RECEIVE_DEBUG
 
-int sent_byte = 0;
-
-pthread_mutex_t recv_lock;
-
-int recv_byte = 0;
-
 int work_done_flag = 0;
 
 int client_thread_num;
@@ -15,17 +9,34 @@ int client_thread_num;
 pthread_mutex_t fin_client_thread_lock;
 int fin_client_thread = 0;
 
-struct response_process_arg {
+struct debug_response_arg {
     struct event * read_ev;
     FILE * fp;
 };
 
+struct response_arg {
+    struct event * read_ev;
+    struct send_info * info;
+};
+
+#define RESPONSE_ARG_SIZE sizeof(struct response_arg)
+
+struct send_info {
+    int * sockfd;
+    pthread_mutex_t * send_lock;
+    int * send_byte;
+    pthread_mutex_t * recv_lock;
+    int * recv_byte;
+};
+
+#define SEND_INFO_SIZE sizeof(struct send_info);
+
 int connect_server(char * server_ip, int port);
 
-void send_request_thread(int fd);
+void send_request_thread(struct send_info * info);
 
 void * send_request(void * arg);
 
-void receive_response_thread(int fd);
+void receive_response_thread(struct send_info * info);
 
 void response_process(int sock, short event, void * arg);
