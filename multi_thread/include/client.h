dff55773864dc4fe268f7a9c59a12968f7a42077
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-01-30 11:26:24 +0800

    [test]

--- multi_thread/include/client.h
+++ multi_thread/include/client.h
@@ -15,6 +15,12 @@ struct response_process_arg {
     FILE * fp;
 };
 
+pthread_mutex_t tot_work_lock;
+int tot_work = 0;
+
+pthread_mutex_t fin_work_lock;
+int fin_work = 0;
+
 int connect_server(char * server_ip, int port);
 
 void send_request_thread(int fd);
