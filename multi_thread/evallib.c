36e059e3787d8b1ef60204b1de3c555c557e8045
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-01-30 21:51:45 +0800

    [test]

--- multi_thread/evallib.c
+++ multi_thread/evallib.c
@@ -15,17 +15,17 @@ void request_end(int byte_sent){
 
     gettimeofday(&end, &tz);
 
-    log(DEBUG, "start sec: %ld, usec: %ld  end sec: %ld, usec: %ld", 
-            start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec);
-
+    log(DEBUG, "start sec: %ld, usec: %ld  end sec: %ld, usec: %ld, byte sent: %d", 
+            start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec, byte_sent);
+/*
     if(end.tv_usec < start.tv_usec){
         end.tv_usec += 1000000;
         end.tv_sec -= 1;
     }
-
+*/
     sec = end.tv_sec - start.tv_sec;
     usec = end.tv_usec - start.tv_usec;
-
+/*
     float start_time = (float)start.tv_sec + ((float)start.tv_usec/(float)1000000.0);
     float end_time = (float)end.tv_sec + ((float)end.tv_usec/(float)1000000.0);
 
@@ -33,6 +33,9 @@ void request_end(int byte_sent){
 
     float tps = (float)handle_request_cnt / elapsed_time;
 //    float thruput = (float)byte_sent / elapsed_time;
+*/
+
+    unsigned long start = 1000000 * start.tv_sec + 
 
     handle_request_cnt++;
 /*
@@ -45,6 +48,9 @@ void request_end(int byte_sent){
 */
 
     char buff[1024];
+
+    printf("start:%f end:%f tot_request: %d tot_byte:%d\n", 
+            start_time, end_time, handle_request_cnt, byte_sent);
 
     sprintf(buff, "start:%f end:%f tot_request: %d tot_byte:%d\n", 
             start_time, end_time, handle_request_cnt, byte_sent);
