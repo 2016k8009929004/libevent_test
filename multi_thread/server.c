111d47113f880b68753bce0bc2ff8b35597d03ac
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-02-11 20:31:45 +0800

    Update server.c

--- multi_thread/server.c
+++ multi_thread/server.c
@@ -214,7 +214,11 @@ void request_process_cb(int fd, short events, void * arg){
 
     strcpy(reply_msg, msg);
 
-    int send_byte_cnt = write(fd, reply_msg, strlen(reply_msg));
+    int send_byte = write(fd, reply_msg, strlen(reply_msg));
+    if(send_byte < 0){
+        perror("[SERVER] send failed");
+        exit(1);
+    }
 
     *(read_arg->byte_sent) += send_byte_cnt;
 
