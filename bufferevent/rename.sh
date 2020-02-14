3a830b46ca1f13b25b57e7ace110a68d1c9c2b76
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-02-14 12:10:12 +0800

    [Update]

--- bufferevent/rename.sh
+++ bufferevent/rename.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-python generate.py && \
-mv perform.txt perf_$1.txt && \
-mv accept_cb.txt pthread_$1.txt && \
-mv request_process_cb.txt callback_$1.txt
+mv accept_cb.txt accept_cb_$1.txt && \
+mv read_cb.txt read_cb_$1.txt
