4db112501739768f5135b5c554432fe805168c66
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-02-27 22:50:02 +0800

    Update calc_cb.py

--- bufferevent/calc_cb.py
+++ bufferevent/calc_cb.py
@@ -3,15 +3,15 @@ import os
 import sys
 
 with open("read_cb.txt", "r") as f:
-    total = 0
+    total = 0.0
     count = 0
     for line in f:
         res = line.split()
-        if len(res) == 2 and res[1].isdigit():
-           total += int(res[1])
+        if len(res) == 2:
+           total += float(res[1])
            count += 1
     f.close()
-    avg = round(float(total)/float(count), 4)
+    avg = round(total/float(count), 4)
 
 print('read_cb average: %.4f' % avg)
 
