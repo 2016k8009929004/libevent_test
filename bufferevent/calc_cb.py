e9ad471a8ef85e087243a6ee1d298922cb1248a3
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-02-27 22:46:45 +0800

    update

--- bufferevent/calc_cb.py
+++ bufferevent/calc_cb.py
@@ -15,19 +15,19 @@ with open("read_cb.txt", "r") as f:
 
 print('read_cb average: %.4f' % avg)
 
-with open("accept_cb.txt", "r") as f:
-    tot_accept = 0
-    tot_pthread_create = 0
-    count = 0
-    for line in f:
-        res = line.split()
-        if len(res) == 4 and res[1].isdigit() and res[3].isdigit():
-            tot_accept += int(res[1])
-            tot_pthread_create += int(res[3])
-            count += 1
-    f.close()
-    avg_accept = round(float(tot_accept)/float(count), 4)
-    avg_pthread_create = round(float(tot_pthread_create)/float(count), 4)
+#with open("accept_cb.txt", "r") as f:
+#    tot_accept = 0
+#    tot_pthread_create = 0
+#    count = 0
+#    for line in f:
+#        res = line.split()
+#        if len(res) == 4 and res[1].isdigit() and res[3].isdigit():
+#            tot_accept += int(res[1])
+#            tot_pthread_create += int(res[3])
+#            count += 1
+#    f.close()
+#   avg_accept = round(float(tot_accept)/float(count), 4)
+#    avg_pthread_create = round(float(tot_pthread_create)/float(count), 4)
 
-print('accept average: %.4f' % (avg_accept))
-print('pthread_create average: %.4f' % (avg_pthread_create))
+#print('accept average: %.4f' % (avg_accept))
+#print('pthread_create average: %.4f' % (avg_pthread_create))
