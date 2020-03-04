c35d91b7236ded990b4f3e08d4d27758b658362e
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-04 20:51:08 +0800

    Update server.c

--- bufferevent_single/server.c
+++ bufferevent_single/server.c
@@ -237,7 +237,7 @@ static void signal_cb(evutil_socket_t sig, short events, void * arg){
 		if(sv_thread[i] == pthread_self()){
 			printf("Server thread %d got signal\n", i);
         }else{
-			pthread_kill(sv_thread[i], signum);
+			pthread_kill(sv_thread[i], sig);
         }
 	}
 
