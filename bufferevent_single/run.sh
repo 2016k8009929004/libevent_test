90d3e60677d1ba08f7c8ad4ccb0f333e762e89f3
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-11 23:17:17 +0800

    Update run.sh

--- bufferevent_single/run.sh
+++ bufferevent_single/run.sh
@@ -13,5 +13,5 @@ then
 #    rm record_core_*.txt
     ./server --num_thread=$2 --num_server_thread=$3 --num_backend_thread=$4
 else
-    ./client --num_thread=$2 $server_ip $server_port
+    ./client $2 $server_ip $server_port
 fi
