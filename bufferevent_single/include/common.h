7293357c236ec50aa0f7d2ae34dddfdbbebe97ba
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-11 21:39:37 +0800

    Update common.h

--- bufferevent_single/include/common.h
+++ bufferevent_single/include/common.h
@@ -41,6 +41,28 @@ struct __attribute__((__packed__)) kv_trans_item {
 
 #define KV_ITEM_SIZE sizeof(struct kv_trans_item)
 
+struct hikv_arg {
+    size_t pm_size;
+    uint64_t num_server_thread;
+    uint64_t num_backend_thread;
+    uint64_t num_warm_kv;
+    uint64_t num_put_kv;
+    uint64_t num_get_kv;
+    uint64_t num_delete_kv;
+    uint64_t num_scan_kv;
+    uint64_t scan_range;
+    uint64_t seed;
+    uint64_t scan_all;
+};
+
+#define HIKV_ARG_SIZE sizeof(struct hikv_arg)
+
+struct server_arg {
+    int core;
+    int thread_id;
+    struct hikv_arg hikv_thread_arg;
+};
+
 #ifndef MAX_CPUS
 #define MAX_CPUS		16
 #endif
