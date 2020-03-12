2f206c231ad3ef8b8c88a848f068f40ee341567c
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-12 08:48:33 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -176,14 +176,11 @@ void * send_request(void * arg){
     gettimeofday(&time1, NULL);
 
     //PUT
-    for(iter = 0;iter < NUM_ITER;iter++){
+    for(iter = 0;iter < 3;iter++){
         if(rand() % 100 <= PUT_PERCENT || iter < NUM_KEYS){
-            printf("===== 1 =====\n");
             snprintf((char *)req_kv->key, KEY_SIZE, "%064llu", key_corpus[key_i]);     //set Key
-            printf("===== 2 =====\n");
 			req_kv->len = VALUE_SIZE;
 			memcpy((char *)req_kv->value, (char *)&value_corpus[key_i * VALUE_SIZE], VALUE_SIZE);   //set Value
-            printf("===== 3 =====\n");
 			key_i = (key_i + 1) & NUM_KEYS_;
 		}else{
 			key_i = rand() & NUM_KEYS_;
@@ -191,17 +188,15 @@ void * send_request(void * arg){
 			req_kv->len = 0;
 			memset((char *)req_kv->value, 0, VALUE_SIZE);
 		}
-        printf("===== 4 =====\n");
 
         if(write(fd, req_kv, KV_ITEM_SIZE) < 0){
 			perror("[CLIENT] send failed");
 	    	exit(1);
     	}
-        printf("===== 5 =====\n");
 
         gettimeofday(&time2, NULL);
         if(time2.tv_sec - time1.tv_sec > 10){
-            printf("[CLIENT] request complete, iter = %d\n", i);
+            printf("[CLIENT] request complete\n");
             break;
         }
     }
