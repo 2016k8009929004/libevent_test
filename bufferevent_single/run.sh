445262e70c5653e12c6f93f3e918719d3443cdfd
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-12 18:04:24 +0800

    Update run.sh

--- bufferevent_single/run.sh
+++ bufferevent_single/run.sh
@@ -8,13 +8,12 @@ client='client'
 server_ip='172.18.13.14'
 server_port=12345
 
-num_put=1048576
-num_get=1048576
+num_opt=1048576
 
 if test $1 = $server
 then
 #    rm record_core_*.txt
-    ./server --num_thread=$2 --num_put=$num_put --num_get=$num_get --num_server_thread=$3 --num_backend_thread=$4
+    ./server --num_warm=$num_opt --num_thread=$2 --num_put=$num_opt --num_get=$num_opt --num_server_thread=$3 --num_backend_thread=$4
 else
     ./client $2 $server_ip $server_port
 fi
