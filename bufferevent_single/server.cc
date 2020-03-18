1602c4935cbf69c862416de51aef52cfcf4585d5
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-18 11:20:50 +0800

    Update server.cc

--- bufferevent_single/server.cc
+++ bufferevent_single/server.cc
@@ -385,7 +385,7 @@ void read_cb(struct bufferevent * bev, void * arg){
             //printf("[SERVER] insert success\n");
 			//sprintf(buff, "[SERVER] PUT success! key: %.*s\nput value: %.*s\n", KEY_SIZE, recv_item->key, VALUE_SIZE, recv_item->value);
             recv_item->len = VALUE_SIZE;
-			sent = mtcp_write(ctx->mctx, sockid, (char *)recv_item, KV_ITEM_SIZE);
+            bufferevent_write(bev, (char *)recv_item, KV_ITEM_SIZE);
 		/*
             sprintf(buff, "[SERVER] PUT success! key: %.*s\n", KEY_SIZE, recv_item->key);
 			fwrite(buff, strlen(buff), 1, fp);
@@ -394,7 +394,7 @@ void read_cb(struct bufferevent * bev, void * arg){
         }else{
 			//sprintf(buff, "[SERVER] PUT failed! key: %.*s\nput value: %.*s\n", KEY_SIZE, recv_item->key, VALUE_SIZE, recv_item->value);
             recv_item->len = -1;
-			sent = mtcp_write(ctx->mctx, sockid, (char *)recv_item, KV_ITEM_SIZE);
+            bufferevent_write(bev, (char *)recv_item, KV_ITEM_SIZE);
 		/*
             sprintf(buff, "[SERVER] PUT failed! key: %.*s\n", KEY_SIZE, recv_item->key);
 			fwrite(buff, strlen(buff), 1, fp);
@@ -407,7 +407,7 @@ void read_cb(struct bufferevent * bev, void * arg){
         if(res == true){
             //printf("[SERVER] get KV item success\n");
             recv_item->len = VALUE_SIZE;
-            sent = mtcp_write(ctx->mctx, sockid, (char *)recv_item, KV_ITEM_SIZE);
+            bufferevent_write(bev, (char *)recv_item, KV_ITEM_SIZE);
 			//sprintf(buff, "[SERVER] GET success! key: %.*s\nget value: %.*s\n", KEY_SIZE, recv_item->key, VALUE_SIZE, recv_item->value);
 		/*
             sprintf(buff, "[SERVER] GET success! key: %.*s\n", KEY_SIZE, recv_item->key);
@@ -417,7 +417,7 @@ void read_cb(struct bufferevent * bev, void * arg){
         }else{
             //printf("[SERVER] get KV item failed\n");
             recv_item->len = -1;
-            sent = mtcp_write(ctx->mctx, sockid, (char *)recv_item, KV_ITEM_SIZE);
+            bufferevent_write(bev, (char *)recv_item, KV_ITEM_SIZE);
 			//sprintf(buff, "[SERVER] GET failed! key: %.*s\nget value: %.*s\n", KEY_SIZE, recv_item->key, VALUE_SIZE, recv_item->value);
 		/*	
             sprintf(buff, "[SERVER] GET failed! key: %.*s\n", KEY_SIZE, recv_item->key);
