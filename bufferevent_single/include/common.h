5c19f7aa65177939454f3295a859f9af70f4a51e
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-19 11:07:04 +0800

    Update common.h

--- bufferevent_single/include/common.h
+++ bufferevent_single/include/common.h
@@ -94,8 +94,8 @@ static int core_limit;
 static pthread_t sv_thread[MAX_CPUS];
 static struct server_arg sv_thread_arg[MAX_CPUS];
 
-static pthread_t cl_thread[MAX_CPUS];
-static struct client_arg cl_thread_arg[MAX_CPUS];
+static pthread_t cl_thread[110];
+static struct client_arg cl_thread_arg[110];
 
 static int done[MAX_CPUS];
 static int cores[MAX_CPUS];
