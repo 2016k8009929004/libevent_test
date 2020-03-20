ed09cb5fa1bfe4da93c0fe694bcc4959b8c497bd
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-20 15:28:47 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -357,7 +357,7 @@ void * send_request(void * arg){
     		memcpy((char *)req_kv->value, (char *)&value_corpus[key_i * VALUE_SIZE], VALUE_SIZE);   //set Value
             //printf(">> req_kv->value: %p, value: %.*s\n", req_kv->value, VALUE_SIZE, req_kv->value);
     		//printf("[CLIENT] PUT key: %llu, value: %.*s\n", key_corpus[key_i], VALUE_SIZE, req_kv->value);
-            printf("[CLIENT] PUT key: %llu\n", key_corpus[key_i]);
+            //printf("[CLIENT] PUT key: %llu\n", key_corpus[key_i]);
 		    key_i = (key_i + 1) % num_put_kv;
 
             put_count++;
