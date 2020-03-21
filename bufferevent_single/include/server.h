c96aeda7e8c571fa194d2cbb27a236d7d2b8d729
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-21 15:29:30 +0800

    Update server.h

--- bufferevent_single/include/server.h
+++ bufferevent_single/include/server.h
@@ -36,7 +36,7 @@ pthread_mutex_t accept_cb_lock;
 #endif
 
 #ifdef __EVAL_READ__
-struct timeval record_start[1500000], record_end[1500000];
+struct timeval record_start[1800000], record_end[1800000];
 pthread_mutex_t read_cb_lock;
 int request_cnt;
 int total_time;
