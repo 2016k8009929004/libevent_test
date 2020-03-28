18786ab709e2b558c756129db523b0ee43cc2ae4
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-03-28 15:32:57 +0800

    Update common.h

--- bufferevent_single/include/common.h
+++ bufferevent_single/include/common.h
@@ -37,8 +37,8 @@
 
 #define PUT_PERCENT 50			// Percentage of PUT operations
 
-#define NUM_KEYS K_512
-#define NUM_KEYS_ K_512_
+#define NUM_KEYS M_1
+#define NUM_KEYS_ M_1_
 
 #define SWAP(a,b) do{a^=b;b^=a;a^=b;}while(0)
 
