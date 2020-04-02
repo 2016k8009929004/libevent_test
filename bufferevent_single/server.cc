f9140d44627afba652086cc241d16103d91d5366
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-02 22:45:24 +0800

    update

--- bufferevent_single/server.cc
+++ bufferevent_single/server.cc
@@ -523,7 +523,7 @@ void read_cb(struct bufferevent * bev, void * arg){
 
 		int i;
 		for(i = 0;i < key_num;i++){
-            printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
+            //printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
 			res = hi->search(thread_id, (uint8_t *)(recv_item + i * KEY_SIZE), (uint8_t *)(value + i * VALUE_SIZE));
 			if(res == false){
                 //printf(" >> GET failed\n");
