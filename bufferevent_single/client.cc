9f46c03eb4fb4138bd594c5e7e56b7c634ec1438
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-02 22:00:41 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -476,7 +476,7 @@ void * send_request(void * arg){
             //printf(">> value_corpus: %p, value: %.*s\n", &value_corpus[key_i * VALUE_SIZE], VALUE_SIZE, &value_corpus[key_i * VALUE_SIZE]);
     		memcpy((char *)req_kv->value, (char *)&value_corpus[key_i * VALUE_SIZE], VALUE_SIZE);   //set Value
             //printf(">> req_kv->value: %p, value: %.*s\n", req_kv->value, VALUE_SIZE, req_kv->value);
-    		//printf("[CLIENT] PUT key: %llu, value: %.*s\n", key_corpus[key_i], VALUE_SIZE, req_kv->value);
+    		printf("[CLIENT] PUT key: %llu, value: %.*s\n", key_corpus[key_i], VALUE_SIZE, req_kv->value);
             //printf("[CLIENT] PUT key: %llu\n", key_corpus[key_i]);
 		    key_i = (key_i + 1) % num_put_kv;
 
@@ -526,12 +526,10 @@ void * send_request(void * arg){
             iter++;
 		} else {
 		//GET
-            printf("[CLIENT] get KV item\n");
             char * key = (char *)malloc(4 * KEY_SIZE);
 
             int send_num;
             for(send_num = 0; (key_j + send_num < num_get_kv) && (send_num < 4);send_num++){
-                printf("key_j: %d, send num: %d, num_get: %d\n", key_j, send_num, num_get_kv);
                 snprintf(key + send_num * KEY_SIZE, KEY_SIZE + 1, "%0llu", key_corpus[key_j + send_num]);     //set Key
             }
 
@@ -566,9 +564,10 @@ void * send_request(void * arg){
 
             int i;
             for(i = 0;i < recv_num;i++){
+                printf("[CLIENT] value: %.*s\n", VALUE_SIZE, req_kv->value);
                 if(strcmp("get failed", value) == 0){
                     //printf("put failed\n");
-                }else if(bufcmp(value + i * VALUE_SIZE, (char *)&value_corpus[(key_j + i) * VALUE_SIZE], VALUE_SIZE)){
+                }else if(bufcmp(value + i * VALUE_SIZE, value_corpus + (key_j + i) * VALUE_SIZE, VALUE_SIZE)){
                     //printf("[CLIENT] GET success! key: %.*s, value: %.*s\n", KEY_SIZE, req_kv->key, VALUE_SIZE, req_kv->value);
                     //printf("[CLIENT] GET success! key: %.*s\n", KEY_SIZE, req_kv->key);
                     match_search++;
