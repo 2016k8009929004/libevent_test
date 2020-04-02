da0154d82b9fcc696253be8f654cc00809744c99
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-02 21:44:39 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -530,7 +530,8 @@ void * send_request(void * arg){
             char * key = (char *)malloc(4 * KEY_SIZE);
 
             int send_num;
-            for(send_num = 0; key_j + send_num < num_get_kv && send_num < 4; send_num++){
+            for(send_num = 0; (key_j + send_num) % num_get_kv < num_get_kv && send_num < 4;send_num++){
+                printf(" >> Set GET key\n");
                 printf(" >> GET key: %.*s\n", KEY_SIZE, key_corpus[key_j + send_num]);
                 snprintf(key + send_num * KEY_SIZE, KEY_SIZE + 1, "%0llu", key_corpus[key_j + send_num]);     //set Key
             }
