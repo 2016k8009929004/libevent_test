d3cad65243c0ef6d0b742a53ac161ee6a8465c75
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-11 00:11:45 +0800

    update

--- bufferevent_single/server.cc
+++ bufferevent_single/server.cc
@@ -523,12 +523,11 @@ void read_cb(struct bufferevent * bev, void * arg){
 
         int i;
 		for(i = 0;i < key_num;i++){
-            //printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
-            char buff[VALUE_LENGTH];
+            printf(" >> GET key: %.*s\n", KEY_SIZE, recv_item + i * KEY_SIZE);
 			res = hi->search(thread_id, (uint8_t *)(recv_item + i * KEY_SIZE), (uint8_t *)(value + i * VALUE_LENGTH));
             if(res == true){
                 //memcpy(value + i * VALUE_LENGTH, buff, VALUE_LENGTH);
-                //printf(" >> GET success! value: %.*s\n", VALUE_LENGTH, value + i * VALUE_LENGTH);
+                printf(" >> GET success! value: %.*s\n", VALUE_LENGTH, value + i * VALUE_LENGTH);
             }else{
                 //printf(" >> GET failed\n");
 	            memset(value + i * VALUE_LENGTH, 0, VALUE_LENGTH);
