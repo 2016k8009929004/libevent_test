5eedd18cea0578b1db40131f34705669c412ed22
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-23 00:15:07 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -822,7 +822,7 @@ int main(int argc, char * argv[]){
     int put_test, get_test, scan_test, scan_range;
     scan_range = 4;
     put_test = get_test = NUM_KEYS;
-    scan_test = NUM_KEYS - 1；
+    scan_test = NUM_KEYS - 1;
 
     struct hikv_arg hikv_thread_arg = {
         20,                                      //pm_size
