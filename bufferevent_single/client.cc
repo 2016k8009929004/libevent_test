d885cea6cf748e85b9ac72cb83b1957be9b32177
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-22 23:54:13 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -22,8 +22,11 @@ void gen_key_corpus(LL * key_corpus, int num_put, int thread_id){
 */
 
     //sequence key
+    LL rand1 = (LL) rand();
+	LL rand2 = (LL) rand();
+	long long sequence = (rand1 << 32) ^ rand2;
     for(key_i = 0; key_i < num_put; key_i ++) {
-		key_corpus[key_i] = (thread_id << 28) | (key_i + 1);
+		key_corpus[key_i] = sequence + key_i;
 		if((char) key_corpus[key_i] == 0) {
 			key_i --;
 		}
