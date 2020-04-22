a131fe0a44c62514d481d1ac18561c9a88d10528
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-22 22:24:55 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -586,7 +586,7 @@ void * send_request(void * arg){
         free(value);
     }
 
-    for(key_k = scan_range;key_k - scan_range < num_scan_kv;){
+    for(key_k = 1;key_k - 1 < num_scan_kv;){
         char * key = (char *)malloc(2 * KEY_SIZE);
 
         snprintf(key, KEY_SIZE, "%0llu", key_corpus[key_k]);     //set Key
