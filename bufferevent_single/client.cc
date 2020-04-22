cafe28f21a791f26d4430343e65d33e5fb3b5321
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-23 00:29:58 +0800

    Update client.cc

--- bufferevent_single/client.cc
+++ bufferevent_single/client.cc
@@ -25,7 +25,7 @@ void gen_key_corpus(LL * key_corpus, int num_put, int thread_id){
     for(key_i = 0; key_i < num_put; key_i ++) {
 		key_corpus[key_i] = (thread_id << 16) + key_i;
         printf(" >> key: %llu\n", key_corpus[key_i]);
-		if((char) key_corpus[key_i] == 0) {
+		if(key_corpus[key_i] == 0) {
 			key_i --;
 		}
 	}
